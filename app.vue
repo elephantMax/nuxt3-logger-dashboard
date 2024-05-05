02ac7c48654517ea86ed19ab7cc1a06fe0d4eaef
elephantMax <hatypovm12@mail.ru>
2024-05-05 13:14:11 +0300

    add nuxt/ui

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex">
+    <div>test 1</div>
+    <div>test 2</div>
+    <UIcon name="i-heroicons-building-library" />
+    <UButton
+      color="cyan"
+      variant="outline"
+    >
+      test
+    </UButton>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/eslint',
-    '@vueuse/nuxt',
-  ],
+  modules: ['@nuxt/eslint', '@vueuse/nuxt', '@nuxt/ui'],
   eslint: {
     config: {
       stylistic: {
