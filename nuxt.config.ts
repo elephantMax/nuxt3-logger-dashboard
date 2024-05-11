e7cc5548f96f6c95d86360c991b55f2c4d874d99
elephantMax <hatypovm12@mail.ru>
2024-05-11 11:28:41 +0300

    add test utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@vueuse/nuxt', '@nuxt/ui'],
+  modules: [
+    '@nuxt/eslint',
+    '@vueuse/nuxt',
+    '@nuxt/ui',
+    '@nuxt/test-utils/module',
+  ],
   eslint: {
     config: {
       stylistic: {
